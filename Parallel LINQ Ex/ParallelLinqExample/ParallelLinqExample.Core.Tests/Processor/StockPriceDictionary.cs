756f03efef5dda6f0bbcffb202d06aa84a405314
DavidWilliams126 <David.williams.126@outlook.com>
2021-02-20 17:49:45 -0500

    GreenTestCode
    got the green test. next to use Linq to pull min and max of TSLA, then to use PLINQ to pull min and max.

--- Parallel LINQ Ex/ParallelLinqExample/ParallelLinqExample.Core.Tests/Processor/StockPriceDictionary.cs
+++ Parallel LINQ Ex/ParallelLinqExample/ParallelLinqExample.Core.Tests/Processor/StockPriceDictionary.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.FileIO;
 
 namespace ParallelLinqExample.Core.Processor
 {
@@ -11,8 +16,25 @@ namespace ParallelLinqExample.Core.Processor
         {
             var file = csvFileName;
             //Load csvfile into dictionary
-            stockHistoryTable = new Dictionary<int, StockPrice>()
-            { {1,new StockPrice("AAP",230.3m) } };
+            stockHistoryTable = new Dictionary<int, StockPrice>();
+            //{ {1,new StockPrice("AAP",230.3m) } };
+            //stockHistoryTable = File.ReadLines(csvFileName).Select(line => line.Split(',')).ToDictionary(line => line[0], line => line[1]);
+            using (TextFieldParser parser = new TextFieldParser(csvFileName))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                int i = 1;
+                while (!parser.EndOfData)
+                {
+                    string[] indRecord = parser.ReadFields();
+                    stockHistoryTable.Add(i, new StockPrice(indRecord[0], Convert.ToDecimal(indRecord[1])));
+                    i++;
+                }
+
+
+
+
+            }
         }
     }
 }
